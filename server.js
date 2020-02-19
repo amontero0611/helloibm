6f2793e54e6f62d4c374e36831b9abf948d3ea85
amontero0611 <amontero@es.ibm.com>
2020-02-19 18:20:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
-console.log('Listening on port ' + process.env.PORT);
+var title = process.env.apptitle ||  ' ';
+var cr = process.env.copyright || ' ';
+var hi = process.env.salutation || ' ';
 //create a server object:
 http.createServer(function (req, res) {
   res.write('IIIIII  BBBBBBB  MM     MM \n'); //write a response to the client
@@ -11,8 +13,8 @@ http.createServer(function (req, res) {
   res.write('  II     BB   BB  MM   MM  \n'); 
   res.write('IIIIII  BBBBBBB  MMM   MMM \n'); 
   res.write(' ');
-  res.write(process.env.apptitle + '\n');
-  res.write(process.env.copyright + '\n');
-  res.write(process.env.salutation + '\n');
+  res.write(title + '\n');
+  res.write(cr + '\n');
+  res.write(hi + '\n');
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080 
