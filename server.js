c7a3f93c2dac5c851d54e970cc9eb1931057bc9b
amontero0611 <amontero@es.ibm.com>
2020-02-19 16:40:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ http.createServer(function (req, res) {
   res.write('  II     BB   BB  MM M MM  \n'); 
   res.write('  II     BB   BB  MM   MM  \n'); 
   res.write('IIIIII  BBBBBBB  MMM   MMM \n'); 
-  res.write(process.env.msg);
+  res.write(' ');
+  res.write(process.env.copyright);
+  res.write(process.env.salutation);
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080 
